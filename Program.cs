8e91450b96d5f2f657aff0c1b069ae0a7d74892d
Stefan Kugler <stefan.kugler@fsla.at>
2017-03-19 17:04:33 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+/*
+ * Remove special characters from pos-printer
+ * https://github.com/stefankugler/print-qrk-json
+ * Stefan Kugler 2017
+ */
+
+using System;
+
+namespace removeesc
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if(args.Length > 0) {
+				//1st argument: full path to json-file
+				string filename = @args[0];
+				
+				try {
+					//Get Path from filename
+					string path = System.IO.Path.GetDirectoryName(@filename);
+					
+					//Debug: Append current filename to debug.txt
+					//System.IO.File.AppendAllText(path + "\\debug.txt", filename + Environment.NewLine);
+					
+					//Read json-file and remove leading and trailing special chars
+					string content = System.IO.File.ReadAllText(@filename);
+					string json = content.Substring(content.IndexOf("{"), content.LastIndexOf("}") - content.IndexOf("{") + 1);
+					
+					//create timestamp for filename and save clean json data
+					string outname = DateTime.Now.ToString("yyyyMMdd-HHmmss");
+					System.IO.File.WriteAllText (@path + "\\" + outname + ".json", json);
+					
+				} catch (Exception ex) {
+					Console.WriteLine(ex.Message);
+					Console.Write("Press any key to continue . . . ");
+					Console.ReadKey(true);
+				}
+			}
+			
+		}
+	}
+}
